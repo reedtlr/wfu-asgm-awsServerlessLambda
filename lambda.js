944cc5e3a99661109813557845a83af2b8b70ecf
tutchings <tutchings.dev@gmail.com>
2020-12-18 15:03:07 -0500

    added awsServerlessExpressMiddleware to lambda

--- lambda.js
+++ lambda.js
@@ -1,7 +1,10 @@
 // lambda.js
 'use strict'
-const awsServerlessExpress = require('aws-serverless-express')
-const app = require('./app')
-const server = awsServerlessExpress.createServer(app)
+const awsServerlessExpress = require('aws-serverless-express');
+const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware');
+const app = require('./app');
+const server = awsServerlessExpress.createServer(app);
 
-exports.handler = (event, context) => { awsServerlessExpress.proxy(server, event, context) }
+app.use(awsServerlessExpressMiddleware.eventContext());
+
+exports.handler = (event, context) => { awsServerlessExpress.proxy(server, event, context) };
